38732db4ee81db7436c914371fbd61ac0ae35505
caroline <karoline.gdn@gmail.com>
2016-10-23 23:28:33 +0000

    initial commit

--- server.js
+++ server.js
@@ -1,10 +1,33 @@
+'use strict';
 var express = require('express');
 var app = express();
+var bodyParser = require('body-parser');
+var routes = require('./app/routes/index.js');
+var api = require('./app/api/timestamp_fcc.js');
 
-app.get('/', function (req, res) {
-  res.send('Hello World!');
+// enables CORS for implementing cross-domain requests
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow", "*");
+    res.header("Access-Control-Allow-Methods", "GET");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Conten-Type, Accept");
+    next();
+    
 });
 
-app.listen(8080, function () {
-  console.log('Example app listening on port 3000!');
+//returns middleware for parsing urlencoded bodies
+app.use(bodyParser.urlencoded({extended: true})); 
+// returns middleware that parses only JSON 
+app.use(bodyParser.json()); 
+
+//shows public folder
+app.use('/public', express.static(process.cwd() + '/public'));
+
+//sets port
+var port = process.env.PORT || 8080;
+
+routes(app);
+api(app);
+
+app.listen(port, function() {
+    console.log('Node.js listening on port ' + port);
 });
